224636526c8ef9a1f7b3b410a4165876ce04c96f
HaiGenBuShang <38555995+HaiGenBuShang@users.noreply.github.com>
2024-01-05 12:38:37 +0800

    seperate 'add user' and 'launch app' action
    Make 'add user' and 'launch app' action more separate a little

--- add_users.sh
+++ add_users.sh
@@ -12,6 +12,5 @@ printf "${user_name}\t${passwd}\t${permission}\n" >> ../UKB_accounts/accounts
 
 chmod 600 ../UKB_accounts/accounts
 
-kill -9 `cat Running_logs/UKB_shiny_pid.txt 2> /dev/null` > /dev/null 2>&1
 chmod +x launch_app.sh
 ./launch_app.sh
--- launch_app.sh
+++ launch_app.sh
@@ -3,6 +3,8 @@
 echo "01 01 * * * cd $(pwd)/Results && find . -name \"*\" -mtime +1 -type f -exec rm -rf {} \;" > remove_file.crontab
 
 crontab remove_file.crontab #remove old file
+
+kill -9 `cat Running_logs/UKB_shiny_pid.txt 2> /dev/null` > /dev/null 2>&1 #stop last running
 nohup R -e "shiny::runApp('./')" > Running_logs/UKB_shiny.log 2>&1 & #run app
 echo $! > Running_logs/UKB_shiny_pid.txt
 
