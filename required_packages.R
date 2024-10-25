a9fab1685f2433e02ea9ae0d0f16c60dffb9db49
HaiGenBuShang <aa4311313@gmail.com>
2024-10-25 10:10:36 +0800

    Update required_packages.R

--- required_packages.R
+++ required_packages.R
@@ -1,2 +1,2 @@
-install.packages("shiny","tidyverse","data.table","rvest","shinymanager","DT","devtools","processx","stringr","waiter","shinyjs","ukbtools","sodium")
+install.packages(c("shiny","tidyverse","data.table","rvest","shinymanager","DT","devtools","processx","stringr","waiter","shinyjs","ukbtools","sodium"))
 devtools::install_github("rmgpanw/ukbwranglr")
